4090465c507b8c38075d51e1eddd11f49de854b4
lukec3x <j.lucasc3x@gmail.com>
2019-06-24 12:08:41 -0300

    centralizando o popup

--- js/trabalhos.js
+++ js/trabalhos.js
@@ -1,10 +1,24 @@
 function abrir(n) {
-    document.getElementById('img').innerHTML = "<img src='" + `img/img_trabalhos_${n}.jpg` + "'>"
+    document.getElementById('light').innerHTML = "<img id='i' src='" + `img/img_trabalhos_${n}.jpg` + "'>"
     document.getElementById('light').style.display= 'block'
     document.getElementById('fade').style.display='block'
+    
+
+    var w = window.screen.width
+    var h = document.body.clientHeight //window.screen.height
+    
+    console.log(w, h, (h * 71 / 100))
+    console.log((screen.width - document.body.clientWidth)/2)
+    console.log(document.body.clientWidth, (document.body.clientWidth / 2) - (document.getElementById('i').width /2))
+    
+    document.querySelector('img').style.height = (h * 71 / 100) + 'px'
+    //document.querySelector('img').style.width = 'auto'
+
+    document.getElementById('light').style.left = ((document.body.clientWidth/2) - (document.getElementById('i').width)/2) + 'px'
 }
 
 function fechar() {
     document.getElementById('light').style.display='none'
     document.getElementById('fade').style.display='none'		
 }
+
