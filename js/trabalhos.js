422514571eb506ec10363889c0f0ca7ee39f6b65
lukec3x <j.lucasc3x@gmail.com>
2019-06-23 22:32:26 -0300

    Incremento de mais imagens

--- js/trabalhos.js
+++ js/trabalhos.js
@@ -1,9 +1,10 @@
-function abrir() {
-    document.getElementById('light').style.display= 'block';
-    document.getElementById('fade').style.display='block';		
+function abrir(n) {
+    document.getElementById('img').innerHTML = "<img src='" + `img/img_trabalhos_${n}.jpg` + "'>"
+    document.getElementById('light').style.display= 'block'
+    document.getElementById('fade').style.display='block'
 }
 
 function fechar() {
-    document.getElementById('light').style.display='none';
-    document.getElementById('fade').style.display='none';		
+    document.getElementById('light').style.display='none'
+    document.getElementById('fade').style.display='none'		
 }
