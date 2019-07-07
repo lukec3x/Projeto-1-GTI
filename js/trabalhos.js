95876101f4909bd540549cb4a29f5126c305a469
lukec3x <j.lucasc3x@gmail.com>
2019-07-07 17:28:31 -0300

    Ajustes

--- js/trabalhos.js
+++ js/trabalhos.js
@@ -1,8 +1,5 @@
 function abrir(n) {
-    document.getElementById('light').innerHTML = "<img id='i' src='" + `img/img_trabalhos_${n}.jpg` + "'>"
-    document.getElementById('light').style.display= 'block'
-    document.getElementById('fade').style.display='block'
-    
+    document.getElementById('light').innerHTML = "<img id='i' src='" + `img/img_trabalhos_${n}.jpg` + "'>" 
 
     var w = window.screen.width
     var h = window.screen.height //document.body.clientHeight
@@ -13,10 +10,23 @@ function abrir(n) {
     console.log((screen.width - document.body.clientWidth)/2)
     console.log(document.body.clientWidth, (document.body.clientWidth / 2) - (document.getElementById('i').width /2))
     
-    document.querySelector('img').style.height = (h * 71 / 100) + 'px'
+    // Pega o valor equivalente a 71% do tamanho da tela
+    var larguraImg = (h * 71 / 100) + 'px'
+    document.querySelector('img').style.height = larguraImg
     //document.querySelector('img').style.width = 'auto'
 
-    document.getElementById('light').style.left = ((document.body.clientWidth/2) - (document.getElementById('i').width)/2) + 'px'
+    // Adiciona oas botões de navegação na imagem
+    document.getElementById('light').innerHTML += "<div id='esq' style='top: 0; background: blue;" + "width: 10%; height: " +
+    larguraImg + "; position: absolute;'></div>" +
+    "<div id='dir' style='left: 90%; top: 0; background: red;" + "width: 10%; height: " + larguraImg +
+    "; position: absolute;'></div>"
+
+    document.getElementById('light').style.display= 'block'
+    document.getElementById('fade').style.display='block'
+
+    //Centraliza o slide
+    document.getElementById('light').style.left = ((document.body.clientWidth /2) - (document.getElementById('i').width) /2) + 'px'
+    document.getElementById('light').style.top = ((h /2) - (document.getElementById('i').height) /2) + 'px'
 }
 
 function fechar() {
