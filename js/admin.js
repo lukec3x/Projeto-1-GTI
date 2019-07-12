4afa554f4c108adc20b2565eb70f9fd4488b2638
lukec3x <j.lucasc3x@gmail.com>
2019-07-12 18:47:26 -0300

    Alterando a Tabela

--- js/admin.js
+++ js/admin.js
@@ -10,21 +10,19 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-var tb = document.getElementById('tb')
+// Fiz a instancia dessa var global para resolver problemas de escopo
+var tb = document.getElementById('tbody')
+var th = document.getElementById('thead')
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         if (user.uid == 'EkRvCmLZSRde5HYHl50IQSlgQ0I2') {
             //alert('Logado')
-            var tbs = tb.style
             var query = firebase.database().ref("dados").orderByKey();
             query.once("value").then(function(snapshot) {
-                var s = `<tr class="tr"><td class='titulo'><span>Nome</span></td><td class='titulo'><span>Email</span></td><td class='titulo'><span>Mensagem</span></td></tr>`
                 snapshot.forEach(function(childSnapshot) {
-                    // Valor da chave
-                    var key = childSnapshot.key
                     var childData = childSnapshot.val()
-                    s += "<tr class=\"tr\">" + 
+                    tb.innerHTML += "<tr class=\"tr\">" + 
                     "<td><input readonly=\"readonly\" value=\"" + childData.nome + "\" type=\"text\"></td>" + 
                     "<td><input readonly=\"readonly\" value=\"" + childData.email + "\" type=\"email\"></td>" +
                     "<td><textarea row=\"1\" readonly=\"readonly\">" + childData.mensagem + "</textarea></td>" +
@@ -32,7 +30,6 @@ firebase.auth().onAuthStateChanged(function (user) {
                 })
             
                 //alert(s)
-                tb.innerHTML = "<table>" + s + "</table>"
             })
         } else {
             alert('Você não tem permissão para acessar essa página')
@@ -58,12 +55,11 @@ function busca(){
     if (!(camp == '')){
         var query = firebase.database().ref("dados").orderByKey();
         query.once("value").then(function(snapshot) {
-            var s = `<tr class="tr"><td class='titulo'><span>Nome</span></td><td class='titulo'><span>Email</span></td><td class='titulo'><span>Mensagem</span></td></tr>`
+            var s = ''
             snapshot.forEach(function(childSnapshot) {
-                var key = childSnapshot.key
                 var db = childSnapshot.val()
                 
-                if(camp == db.nome.toUpperCase() || camp == db.email.toUpperCase() || camp == db.mensagem.toUpperCase()){
+                if (camp == db.nome.toUpperCase() || camp == db.email.toUpperCase() || camp == db.mensagem.toUpperCase()) {
                     //console.log(key + " " + db.nome.toUpperCase())
                     
                     s += "<tr class=\"tr\">" + 
@@ -71,13 +67,12 @@ function busca(){
                     "<td><input readonly=\"readonly\" value=\"" + db.email + "\" type=\"email\"></td>" +
                     "<td><textarea row=\"1\" readonly=\"readonly\">" + db.mensagem + "</textarea></td>" +
                     "</tr>"
-    
                 }
             })
-            if(s == `<tr class="tr"><td class='titulo'><span>Nome</span></td><td class='titulo'><span>Email</span></td><td class='titulo'><span>Mensagem</span></td></tr>`){
+            if(s == ''){
                 s = '<p>Não Encontrado!</p>'
             }
-            tb.innerHTML = "<table>" + s + "</table>"
+            tb.innerHTML = s
         })
     }
 }
